34bb6bda275b0a5d69056b1d423dc401723a7426
jint <jint@csc.com.cn>
2021-07-26 14:00:11 +0800

    init

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+//actions允许我们创建回调来决定当点击事件发生时 Storybook UI 的actions面板将如何显示。
+//这样当我们创建了一个 pin 按钮后，我们就可以在测试 UI 中查看该按钮是否被点击成功了。 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
